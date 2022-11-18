7b74b5b6ce9fc5b685e11f48b28b476d6ca47e6a
JP Guiang <107418008+jp-guiang@users.noreply.github.com>
2022-11-19 11:25:40 +1300

    Round and f-string formatting for calculator

--- calculator.py
+++ calculator.py
@@ -3,3 +3,16 @@ y=int(input('What is y? '))
 
 
 print(x+y)
+
+a=float(input('What is a?: '))
+b=float(input('What is b?: '))
+
+print(a+b)
+
+c=a+b
+c=round(c,2) # Round(number, dp)
+
+print(c)
+
+d=1000
+print(f'{d:,}') # use :, to separate numbers with 'z'
