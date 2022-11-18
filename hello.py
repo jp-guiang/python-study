1cc8325c65120ee8e9f477430a8b06845c912860
JP Guiang <107418008+jp-guiang@users.noreply.github.com>
2022-11-18 23:48:40 +1300

    string methods

--- hello.py
+++ hello.py
@@ -1,8 +1,17 @@
 print('Hello world')
 
+#assign input to a variable
 name=input("What's your name? ")
 
+
+
+#print with variable
 print("Hello", name)
 
 # f-string
 print(f'Hello, {name}, but in f-string')
+
+# remove whitespace from variable
+stripVar="     test      "
+stripVar=stripVar.strip()
+print(f'This variable should have no extra white space: {stripVar}')
