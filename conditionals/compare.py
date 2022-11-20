40237d82077ae5fc615c0fd8684bc222a64cf22e
JP Guiang <107418008+JP-Guiang@users.noreply.github.com>
2022-11-20 22:48:57 +1300

    Refactor less conditionst

--- conditionals/compare.py
+++ conditionals/compare.py
@@ -1,9 +1,23 @@
 x=int(input('What is x?: '))
 y=int(input('What is y?: '))
 
-if x<y:
-  print('x is less than y')
-elif x>y:
-  print('x is greater than y')
-elif x==y:
+# if x<y:
+#   print('x is less than y')
+# elif x>y:
+#   print('x is greater than y')
+# else:
+#   print('x is equal to y')
+
+# REFACTOR
+# if x>y or x<y:
+#   print('x is not equal to y')
+# else:
+#   print('x is equal to y')
+
+# REFACTOR
+if x!=y:
+  print('x is not equal to y')
+else:
   print('x is equal to y')
+
+  
